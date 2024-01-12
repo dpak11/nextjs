c9ccc24aadc408a988c9e54dc2fe5fc8bde1f7f7
deepak <deepakb@protonmail.com>
2024-01-12 11:09:16 +0530

    added tasklist

---
+++ tasklist/src/app/newtask/page.js
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export default function NewTask() {
+  return (
+    <div>Create a New Task</div>
+  )
+}
--- tasklist/src/app/page.js
+++ tasklist/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
-      <h1>Task List</h1>
+      <h1>Welcome to Tasks Home Page</h1>
+      <Link href="/tasklist">Show List of all tasks</Link> | 
+      <Link href="/newtask">Create New Task</Link>
     </>
   );
 }
